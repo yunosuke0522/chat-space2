64f0a92b016bfbbacd3575d38125ae3bfed22a50
yunosuke0522 <yunosuke.otake@di-v.co.jp>
2020-04-28 20:59:54 +0900

    ユーザー管理
    devise

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,13 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace2
   class Application < Rails::Application
-    config.load_defaults 6.0
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
+      config.i18n.default_locale = :ja
+      config.time_zone = 'Tokyo'
     end
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
   end
 end
