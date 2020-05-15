9d71c3c1a0e93688ef840f296af8fd88671afe89
yunosuke0522 <yunosuke.otake@di-v.co.jp>
2020-05-15 19:24:05 +0900

    自動更新

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -3,7 +3,7 @@ $(function(){
   function buildHTML(message){
     image = ( message.image ) ? `<img class="message__image" src=${message.image} >` : "";
       let html =
-        `<div class="message">
+        `<div class="message"data-message-id=${message.id}>
           <div class="message__upper-info">
             <div class="message__upper-name">
               ${message.user_name}
@@ -22,35 +22,33 @@ $(function(){
       return html; 
   }
 
-  $('.form__new-message').on('submit', function(e){
-    e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
+reloadMessages = function() {
+  let last_message_id = $('.message:last').data("message-id");
 
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
+  console.log(last_message_id)
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
 
-    .done(function(data){
-      console.log("ok")
-      let html = buildHTML(data);
-      $('.messages').append(html);    
-      $('form')[0].reset();
+  .done(function(messages) {
+    if (messages.length !== 0) {
+      let insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-    })
-    
-    .fail(function() {
-      alert("メッセージ送信に失敗しました");
-    })
 
-    .always(function(){            
-      $('.submit-btn').prop("disabled", false);
-    });
-
+    }
+  })
+  .fail(function() {
+    alert('error');
   });
+};
+setInterval(reloadMessages, 7000);
+
+
 });
